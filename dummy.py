0b7aa9c8227d774869d38eae6f1c759a9e2a974b
Joël Perras <joel@nerderati.com>
2019-07-02 14:22:03 -0400

    Basic script registration.

---
+++ dummy.py
@@ -0,0 +1,6 @@
+import weechat
+
+weechat.register("dummy", "Dummy", "1.0", "MIT", "Highlight leading non-slash characters input text.", "", "")
+weechat.prnt("", "Hello, from dummy python script!")
+
+
